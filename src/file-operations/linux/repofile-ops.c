79cbb68f1182b148ee30364b0915649de3233ec0
huyoufumac <huyoufu1988@163.com>
2021-04-10 18:20:00 +0800

    modify CMakeLists.txt to adapter multiple platform

--- src/file-operations/linux/repofile-ops.c
+++ src/file-operations/linux/repofile-ops.c
@@ -3,4 +3,10 @@
  * contact me -->https://www.jk1123.com
  */
 #include "repofile-ops.h"
+#include "repofile-ops.h"
+#include <stdio.h>
+void rfil_collect(char* repo_dir,RepoFileInfoList* repoFileInfoList){
+    printf("正在收集数据!!");
 
+
+}
--- src/file-operations/unix/repofile-ops.c
+++ src/file-operations/unix/repofile-ops.c
@@ -3,4 +3,9 @@
  * contact me -->https://www.jk1123.com
  */
 #include "repofile-ops.h"
+#include <stdio.h>
+void rfil_collect(char* repo_dir,RepoFileInfoList* repoFileInfoList){
+    printf("正在收集数据!!");
 
+
+}
